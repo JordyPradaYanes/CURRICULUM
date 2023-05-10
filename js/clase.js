c092bc98155e7b3e8fec77e28dfea39eed675514
JordyPradaYanes <jdpradayufpso.edu.co>
2023-05-10 15:59:22 -0500

    learning Js

---
+++ js/clase.js
@@ -0,0 +1,37 @@
+//variables
+const nameUser ="Jordy Prada";
+var x = "valor";
+let y = "valor";
+
+let a = 20;
+let b= 5.4; 
+let String = "Mi nombre es Jordy Prada";
+let number = "0123456789";
+let boolean_uno = false;
+let boolean_dos = true;
+let array = [1,2,3,4,5];
+let array_string = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"];
+
+//Operadore aritmeticos
+
+let suma = 1+5+a;
+let resta = 1-5-a;
+let multi = 1*5*a;
+let divi = 1/5/a;
+let module = 10%2;
+let elev = 5^2;
+console.log(suma+"\n"+ resta+"\n"+multi+"\n"+divi+"\n"+module+"\n"+elev);
+
+//operadores de comparacion = / == / === / <> / >= / <= / > / <
+//operadores logicos AND && / OR || /
+if(10>5){
+    for(let i=0;i<array_string.length;i++){
+        console.log(array_string[i]);
+    }
+}
+let j = 5;
+let k = 10;
+while (k>=j){
+    console.log("Entra")
+    j++;
+}
